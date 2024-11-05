5a82575bc179f4e804898fce57f460914d2f08f8
andresrq94 <andres.roldan.94@gmail.com>
2024-11-05 17:45:17 +0100

    Update and rename test_query2.sql to scripts_test/test_query2.sql

---
+++ scripts_test/test_query2.sql
@@ -0,0 +1,13 @@
+﻿INSERT INTO staging_customer (customer_id, personal_id, country, year_of_birth, income_range, first_name, last_name)
+	VALUES 
+		(1, '3297172JZ','GERMANY', 2001, 'REPLACE_OLD', 'JESSICA_EMMA', 'RODRIGUEZ'),
+		(199999999, '9999999XX','CANADA', 1990, 'REPLACE_OLD', 'JOSE', 'LALALA');
+
+INSERT INTO staging_product (product_id, company, premium, product_category, product_detail)
+	VALUES 
+		(1, 'PRUDENTIAL', 109, 'REPLACE', 'REPLACE'),
+		(999, 'TEST_1', 150, 'REPLACE', 'REPLACE'),
+		(888, 'TEST_2.', 900, 'TEST_2', 'TEST_2');
+
+
+EXEC IncrementalLoad_All_Tables;
